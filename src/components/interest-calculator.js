f20be93863153342c29e9c3db375d437b62f8fe1
Allister Rooke <allisterg@gmail.com>
2018-11-12 11:34:21 -0500

    added dispatch handlers to inputs onchange

--- src/components/interest-calculator.js
+++ src/components/interest-calculator.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import React from 'react';
+import {setPrincipal, setYears, setInterest} from '../actions'
 
 // Connect this component
 function InterestCalculator(props) {
@@ -8,17 +9,19 @@ function InterestCalculator(props) {
             onSubmit={e => e.preventDefault()}>
             <div className="form-group">
                 <label htmlFor="principal">Principal ($)</label>
-                <input type="number" id="principal" value={props.principal}
+                <input onChange={(e) => props.dispatch(setPrincipal(Number(e.target.value)))} type="number" id="principal" value={props.principal}
                     min="0" />
             </div>
             <div className="form-group">
                 <label htmlFor="interest">Interest rate (%)</label>
                 <input type="number" id="interest" value={props.interest}
+                    onChange={(e) => props.dispatch(setInterest(Number(e.target.value)))}
                     min="0" max="100" step="0.1" />
             </div>
             <div className="form-group">
                 <label htmlFor="years">Years</label>
                 <input type="number" id="years" value={props.years}
+                    onChange={(e) => props.dispatch(setYears(Number(e.target.value)))}
                     min="0" max="100" />
             </div>
             <div className="form-group">
